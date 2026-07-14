dcf59de029a23654bb613a0bcee971172769404f
lizclord <lizz.yu@gmail.com>
2026-07-14 21:01:48 +0100

    Update route.js

--- app/api/send-email/route.js
+++ app/api/send-email/route.js
@@ -8,7 +8,7 @@ export async function POST(request) {
   try {
     await sgMail.send({
       to,
-      from: 'YOUR_SHARED_EMAIL_HERE',
+      from: 'lizzandtheman@gmail.com',
       subject,
       text,
     })
